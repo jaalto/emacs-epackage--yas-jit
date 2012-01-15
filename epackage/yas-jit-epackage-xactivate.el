e39f20fc22f4a87d967146f123fdb66b16694df2
Jari Aalto <jari.aalto@cante.net>
2012-01-15 23:40:27 +0200

    epackage/: *-epackage-*
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/yas-jit-epackage-xactivate.el
+++ epackage/yas-jit-epackage-xactivate.el
@@ -1,14 +1,14 @@
 (let ((dir "~/.emacs.d/epackage/packages/yasnippet/snippets"))
   (cond
    ((not (file-directory-p dir))
-    (message "[ERROR] yas-jit-epkg-xactivate: Not exists %s" dir))
+    (message "[ERROR] yas-jit-epackage-xactivate: Not exists %s" dir))
    ((or (not (boundp 'yas/root-directory))
 	(not (stringp yas/root-directory)))
     (setq yas/root-directory dir)
     (yas/get-jit-loads))
    ((message
-     (concat "[WARN] yas-jit-epkg-xactivate: Not activating, "
+     (concat "[WARN] yas-jit-epackage-xactivate: Not activating, "
 	     "yas/root-directory already set: %s")
      yas/root-directory))))
 
-(provide 'yas-jit-epkg-xactivate)
+(provide 'yas-jit-epackage-xactivate)
